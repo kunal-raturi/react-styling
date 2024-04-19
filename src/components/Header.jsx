9f36e869cc9863bd0070c7af2bc1da5c6ff12478
kunal <kunalraturi1@gmail.com>
2024-04-19 23:30:23 +0530

    inline styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p
+        style={{
+          color: "red",
+        }}
+      >
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
